009f72e2ee423148e017c04a00a791459dfef83f
thoaiky1992 <thoaiky1992@gmail.com>
2020-05-06 15:17:10 +0700

    update

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-
-app.engine('html',require('ejs').renderFile);
-app.set('view engine','html');
-app.set('views','./');
-app.use(express.static('./public'))
+const fs = require('fs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', './');
+app.use(express.static('./public'));
 app.listen(3000);
-app.get('/',(req,res) => {
-	res.render('mp3.html');
-})
+app.get('/', (req, res) => {
+  res.render('mp3.html');
+  fs.readFile('db.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    let result = JSON.parse(data);
+    result.users.push({ name: 'Tran Hieu', age: 20 });
+    // console.log(result)
+    fs.writeFile('db.json', JSON.stringify(result), 'utf8',function(){
+		
+	});
+  });
+});
